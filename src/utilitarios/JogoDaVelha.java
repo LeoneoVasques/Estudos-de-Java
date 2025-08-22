0a880f559464f9ec9e5ee14a6d537084e6883434
Leoneo <leoneo.vasques@gmail.com>
2025-08-22 14:58:32 -0300

    ajustando jogo

--- src/utilitarios/JogoDaVelha.java
+++ src/utilitarios/JogoDaVelha.java
@@ -1,98 +1,79 @@
 import java.util.Scanner;
 
 public class JogoDaVelha {
-        static char vb = ' ';
-        static char p11 = vb, p12 = vb, p13 = vb, p21 = vb, p22 = vb, p23 = vb, p31 = vb, p32 = vb, p33 = vb;
-    public static void main(String[] args) {
-        int coluna, linha;
-        int repeat = 1;
-        Scanner sc = new Scanner(System.in);
-        while (repeat == 1) {
-            show();
-            System.out.printf("Bem vindo ao jogo da velha!%n");
-            System.out.printf("Jogador 1, insira o número da linha: %n");
-            linha = sc.nextInt();
-            System.out.printf("Jogador 1, insira o número da coluna: %n");
-            coluna = sc.nextInt();
-            if (linha == 1 && coluna == 1) {p11 = 'X';}
-            if (linha == 1 && coluna == 2) {p12 = 'X';}
-            if (linha == 1 && coluna == 3) {p13 = 'X';}
-            if (linha == 2 && coluna == 1) {p21 = 'X';}
-            if (linha == 2 && coluna == 2) {p22 = 'X';}
-            if (linha == 2 && coluna == 3) {p23 = 'X';}
-            if (linha == 3 && coluna == 1) {p31 = 'X';}
-            if (linha == 3 && coluna == 2) {p32 = 'X';}
-            if (linha == 3 && coluna == 3) {p33 = 'X';}
-            show();
-            System.out.printf("Jogador 2, insira o número da linha: %n");
-            linha = sc.nextInt();
-            System.out.printf("Jogador 2, insira o número da coluna: %n");
-            coluna = sc.nextInt();
-            if (linha == 1 && coluna == 1) {p11 = 'O';}
-            if (linha == 1 && coluna == 2) {p12 = 'O';}
-            if (linha == 1 && coluna == 3) {p13 = 'O';}
-            if (linha == 2 && coluna == 1) {p21 = 'O';}
-            if (linha == 2 && coluna == 2) {p22 = 'O';}
-            if (linha == 2 && coluna == 3) {p23 = 'O';}
-            if (linha == 3 && coluna == 1) {p31 = 'O';}
-            if (linha == 3 && coluna == 2) {p32 = 'O';}
-            if (linha == 3 && coluna == 3) {p33 = 'O';}
-            show();
-            System.out.printf("Jogador 1, insira o número da linha: %n");
-            linha = sc.nextInt();
-            System.out.printf("Jogador 1, insira o número da coluna: %n");
-            coluna = sc.nextInt();
-            if (linha == 1 && coluna == 1) {p11 = 'X';}
-            if (linha == 1 && coluna == 2) {p12 = 'X';}
-            if (linha == 1 && coluna == 3) {p13 = 'X';}
-            if (linha == 2 && coluna == 1) {p21 = 'X';}
-            if (linha == 2 && coluna == 2) {p22 = 'X';}
-            if (linha == 2 && coluna == 3) {p23 = 'X';}
-            if (linha == 3 && coluna == 1) {p31 = 'X';}
-            if (linha == 3 && coluna == 2) {p32 = 'X';}
-            if (linha == 3 && coluna == 3) {p33 = 'X';}
-            show();
-            System.out.printf("Jogador 1, insira o número da linha: %n");
-            linha = sc.nextInt();
-            System.out.printf("Jogador 1, insira o número da coluna: %n");
-            coluna = sc.nextInt();
-            if (linha == 1 && coluna == 1) {p11 = 'X';}
-            if (linha == 1 && coluna == 2) {p12 = 'X';}
-            if (linha == 1 && coluna == 3) {p13 = 'X';}
-            if (linha == 2 && coluna == 1) {p21 = 'X';}
-            if (linha == 2 && coluna == 2) {p22 = 'X';}
-            if (linha == 2 && coluna == 3) {p23 = 'X';}
-            if (linha == 3 && coluna == 1) {p31 = 'X';}
-            if (linha == 3 && coluna == 2) {p32 = 'X';}
-            if (linha == 3 && coluna == 3) {p33 = 'X';}
-            show();
-            System.out.printf("Jogador 1, insira o número da linha: %n");
-            linha = sc.nextInt();
-            System.out.printf("Jogador 1, insira o número da coluna: %n");
-            coluna = sc.nextInt();
-            if (linha == 1 && coluna == 1) {p11 = 'X';}
-            if (linha == 1 && coluna == 2) {p12 = 'X';}
-            if (linha == 1 && coluna == 3) {p13 = 'X';}
-            if (linha == 2 && coluna == 1) {p21 = 'X';}
-            if (linha == 2 && coluna == 2) {p22 = 'X';}
-            if (linha == 2 && coluna == 3) {p23 = 'X';}
-            if (linha == 3 && coluna == 1) {p31 = 'X';}
-            if (linha == 3 && coluna == 2) {p32 = 'X';}
-            if (linha == 3 && coluna == 3) {p33 = 'X';}
-            show();
-            if (p11 == p12 && p12 == p13) {
-                System.out.printf("Pabens");
+    static public int jogador = 1;
+    static boolean win  = false;
+    static char[][] tabuleiro = new char[3][3];
+
+    public void setTabuleiro() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tabuleiro[i][j] = ' ';
             }
-            p11 = vb; p12 = vb; p13 = vb; p21 = vb; p22 = vb; p23 = vb; p31 = vb; p32 = vb; p33 = vb;
-            System.out.printf("Pressione 1 para jogar novamente: ");
-            repeat = sc.nextInt();
-
         }
     }
 
-    public static void show() {
-                System.out.printf("| %c | %c | %c |%n", p11, p12, p13);
-                System.out.printf("| %c | %c | %c |%n", p21, p22, p23);
-                System.out.printf("| %c | %c | %c |%n", p31, p32, p33);
+    public void showTabuleiro(){
+        System.out.printf("| %c | %c | %c |%n", tabuleiro[0][0], tabuleiro[0][1], tabuleiro[0][2]);
+        System.out.printf("| %c | %c | %c |%n", tabuleiro[1][0], tabuleiro[1][1], tabuleiro[1][2]);
+        System.out.printf("| %c | %c | %c |%n", tabuleiro[2][0], tabuleiro[2][1], tabuleiro[2][2]);
+
+    }
+    public void verifywin(){
+            if (1 == 1){
+            win = false;
+    }
+    }
+
+    public void jogada(int x, int y){
+        int qual_jogador = jogador % 2;
+            if (qual_jogador != 0) {
+                if (x == 1 && y == 1) tabuleiro[0][0] = 'X';
+                if (x == 1 && y == 2) tabuleiro[0][1] = 'X';
+                if (x == 1 && y == 3) tabuleiro[0][2] = 'X';
+
+                if (x == 2 && y == 1) tabuleiro[1][0] = 'X';
+                if (x == 2 && y == 2) tabuleiro[1][1] = 'X';
+                if (x == 2 && y == 3) tabuleiro[1][2] = 'X';
+
+                if (x == 3 && y == 1) tabuleiro[2][0] = 'X';
+                if (x == 3 && y == 2) tabuleiro[2][1] = 'X';
+                if (x == 3 && y == 3) tabuleiro[2][2] = 'X';
+                jogador++;
+            }
+            else if (qual_jogador == 0) {
+                if (x == 1 && y == 1) tabuleiro[0][0] = 'O';
+                if (x == 1 && y == 2) tabuleiro[0][1] = 'O';
+                if (x == 1 && y == 3) tabuleiro[0][2] = 'O';
+
+                if (x == 2 && y == 1) tabuleiro[1][0] = 'O';
+                if (x == 2 && y == 2) tabuleiro[1][1] = 'O';
+                if (x == 2 && y == 3) tabuleiro[1][2] = 'O';
+
+                if (x == 3 && y == 1) tabuleiro[2][0] = 'O';
+                if (x == 3 && y == 2) tabuleiro[2][1] = 'O';
+                if (x == 3 && y == 3) tabuleiro[2][2] = 'O';
+                jogador++;
+        }
+        else{
+            System.out.println("jogador invalido");
         }
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        JogoDaVelha jogo = new JogoDaVelha();
+        System.out.printf("Bem vindo ao jogo da velha!%n");
+        jogo.setTabuleiro();
+        while (!win) {
+            jogo.showTabuleiro();
+            System.out.printf("Jogador %d, insira o número da linha: %n", jogador);
+            int linha = sc.nextInt();
+            System.out.printf("Jogador %d, insira o número da coluna: %n", jogador);
+            int coluna = sc.nextInt();
+            jogo.jogada(linha, coluna);
+            jogo.verifywin();
+        }
+            sc.close();
+    }
+}
